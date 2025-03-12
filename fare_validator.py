bd37042492f9c6ccea2ee03f51b37705b99f00a7
djf72904 <djfaulhaber29@gmail.com>
2025-03-12 17:11:50 -0400

    completed fare validation for mtaBus

--- fare_validator.py
+++ fare_validator.py
@@ -34,15 +34,15 @@ class fare_validator:
             self.fix_septaBus_fares()
 
         # Fare based off zones
-        elif self.unzipped_path == "Metro_North":
-            self.fix_metroNorth_fares()
-
         elif self.unzipped_path == "LIRR":
             self.fix_lirr_fares()
 
         # Hard to fix
         elif self.unzipped_path == "NJT_Rail":
             self.fix_njtRail_fares()
+
+        elif self.unzipped_path == "Metro_North":
+            self.fix_metroNorth_fares()
 
         elif self.unzipped_path == "NJT_Bus":
             self.fix_njtBus_fares()
@@ -59,7 +59,7 @@ class fare_validator:
         os.remove(path)
         os.rename(f"./{self.unzipped_path}_fare_attributes_temp.csv", path)
 
-        print("Updated PATH fare_attributes.txt")
+        print(f"Updated {self.unzipped_path} fare_attributes.txt")
 
     def fix_nycFerry_fares(self):
         path = f"./transit_datasets_unzipped/{self.unzipped_path}/fare_attributes.txt"
@@ -73,39 +73,90 @@ class fare_validator:
         os.remove(path)
         os.rename(f"./{self.unzipped_path}_fare_attributes_temp.csv", path)
 
-        print("Updated NYC_Ferry fare_attributes.txt")
+        print(f"Updated {self.unzipped_path} fare_attributes.txt")
 
     def fix_mtaSubway_fares(self):
         path = f"./transit_datasets_unzipped/{self.unzipped_path}/fare_attributes.txt"
         if not os.path.exists(path):
-            mta_fares = {
-                "fare_id": [1],
+            subway_fares = {
+                "fare_id": ["single_fare"],
                 "price": [2.90],
                 "currency_type": ["USD"],
                 "payment_method": [1],
                 "agency_id": [self.unzipped_path],
                 "transfers": [None]
             }
-            df = pd.DataFrame(mta_fares)
+            df = pd.DataFrame(subway_fares)
             df.to_csv(path, index = False)
-            print("Updated MTA_Subway fare_attributes.txt")
+            print(f"Created {self.unzipped_path} fare_attributes.txt")
         else:
-            print("Error updating fare_attributes.txt in MTA_Subway. File already exists")
+            print(f"Error updating fare_attributes.txt in {self.unzipped_path}. File already exists")
 
     def fix_mtaBus_fares(self):
-        print("MTA_Bus express vs local for fares")
+        path = f"./transit_datasets_unzipped/{self.unzipped_path}/fare_attributes.txt"
+        if not os.path.exists(path):
+            bus_fares = {
+                "fare_id": ["local_fare", "express_fare"],
+                "price": [2.90, 7.00],
+                "currency_type": ["USD", "USD"],
+                "payment_method": [0, 0],
+                "agency_id": [self.unzipped_path, self.unzipped_path],
+                "transfers": [None, None],
+                "transfer_duration": [7200, 7200]
+            }
+            df = pd.DataFrame(bus_fares)
+            df.to_csv(path, index = False)
+            print(f"Created {self.unzipped_path} fare_attributes.txt")
+        else:
+            print(f"Error updating fare_attributes.txt in {self.unzipped_path}. File already exists")
+
+        path = f"./transit_datasets_unzipped/{self.unzipped_path}/routes.txt"
+
+        df = pd.read_csv(path, sep=",")
+        route_names = df["route_long_name"].values
+        route_ids = df["route_id"].values
+        fare_ids = []
+
+        for i in range(len(df)):
+            if "express" in route_names[i].lower():
+                fare_ids.append("express_fare")
+            else:
+                fare_ids.append("local_fare")
+
+        path = f"./transit_datasets_unzipped/{self.unzipped_path}/fare_rules.txt"
+        if not os.path.exists(path):
+            df = pd.DataFrame({"route_id": route_ids, "fare_id": fare_ids})
+            df.to_csv(path, index=False)
+            print(f"Created {self.unzipped_path} fare_rules.txt")
+        else:
+            print(f"Error updating fare_rules.txt in {self.unzipped_path}. File already exists")
 
     def fix_septaBus_fares(self):
-        print("SEPTA_Bus based off zones for fares")
-
-    def fix_metroNorth_fares(self):
-        print("Metro_North based off zones for fares")
+        path = f"./transit_datasets_unzipped/{self.unzipped_path}/fare_attributes.txt"
+        if not os.path.exists(path):
+            bus_fares = {
+                "fare_id": ["single_fare"],
+                "price": [2.50],
+                "currency_type": ["USD"],
+                "payment_method": [0],
+                "agency_id": [self.unzipped_path],
+                "transfers": [2],
+                "transfer_duration": [7200]
+            }
+            df = pd.DataFrame(bus_fares)
+            df.to_csv(path, index=False)
+            print(f"Created {self.unzipped_path} fare_attributes.txt")
+        else:
+            print(f"Error updating fare_attributes.txt in {self.unzipped_path}. File already exists")
 
     def fix_lirr_fares(self):
         print("LIRR based off zones for fares")
 
     def fix_njtRail_fares(self):
         print("Must implement web scraping for NJT_Rail")
+
+    def fix_metroNorth_fares(self):
+        print("Metro_North based off zones for fares. complicated")
 
     def fix_njtBus_fares(self):
         print("FUCK NJT BUS")
